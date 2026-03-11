6b3b604c82cf84869395c7fad694e3d922d9ad18
JohanMattsson <johan.mattsson@goteborgsregionen.se>
2026-03-11 15:12:42 +0100

    Fixing dockerstart

--- ps_project/backend/docker-entrypoint.sh
+++ ps_project/backend/docker-entrypoint.sh
@@ -7,4 +7,14 @@ if [ -d /opt/admin-build ]; then
   cp -a /opt/admin-build/. /app/web/admin/
 fi
 
-exec make build run
+APP_BINARY_NAME="${APP_BINARY_NAME:-projektps}"
+APP_BINARY_ENTRYPOINT="${APP_BINARY_ENTRYPOINT:-./cmd/server}"
+BUILD_NUMBER="$(date -u +%Y%m%d.%H%M%S)"
+
+go build \
+  -o "${APP_BINARY_NAME}" \
+  -buildvcs=false \
+  -ldflags "-X main.serverrunner=GoDocker -X main.environment= -X main.buildnumber=${BUILD_NUMBER} -X main.buildgreeter=none -X main.buildtrigger=none" \
+  "${APP_BINARY_ENTRYPOINT}"
+
+exec "./${APP_BINARY_NAME}"
